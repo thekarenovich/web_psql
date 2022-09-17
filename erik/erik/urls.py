f66933d85cfa62b5b3b7c3c8104f14f707c772ac
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-09-18 01:15:03 +0300

    Update urls.py

--- erik/erik/urls.py
+++ erik/erik/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('', include("main.urls")),
     path('news/', include("news.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
